2ba2f1aaf8533f83a57ae3b3794cadf0d60e6732
Filipescordeiro2 <filipescordeiro@hotmail.com>
2025-06-05 13:48:05 -0300

    start

---
+++ barberService/src/main/java/com/fscordeiro/barberService/BarberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fscordeiro.barberService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberServiceApplication.class, args);
+	}
+
+}
---
+++ barberService/src/test/java/com/fscordeiro/barberService/BarberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fscordeiro.barberService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
